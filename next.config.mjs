1ce27938a513677ec6953c0235b06d22c5a51c1d
Hilal6886 <ratherhilal68@gmail.com>
2024-12-06 23:12:43 +0530

    Init t

--- next.config.mjs
+++ next.config.mjs
@@ -6,18 +6,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-const { execSync } = require('child_process');
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      try {
-        execSync('npx prisma generate');
-        console.log('Prisma Client successfully generated.');
-      } catch (error) {
-        console.error('Failed to generate Prisma Client:', error);
-      }
-    }
-    return config;
-  },
-};
